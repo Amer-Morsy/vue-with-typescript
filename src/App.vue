98f534915b3239bcca0aeb872041a33ace1c7eff
Amer-Morsy <amermousa341@gmail.com>
2025-04-05 03:49:00 +0300

    lesson-4

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,18 @@
 <template>
   <div class="app">
-    <!-- <p>{{ name }} {{ age }}</p>
-    <button @click="changeName('Zelda')">change name</button>
-    <button @click="changeAge('34')">change age</button> -->
-    <h1>Jobs</h1>
-    <ul>
-      <li v-for="job in jobs" :key="job.id">
-        <h2>{{ job.title }}</h2>
-        <p>Location: {{ job.location }}</p>
-        <p>Salary: {{ job.salary }}</p>
-      </li>
-    </ul>
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, toRefs } from "vue";
+import { defineComponent, ref } from "vue";
 import Job from "@/types/Job";
+import JobList from "@/components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
-    // const state = reactive({
-    //   name: "Link",
-    //   age: 25 as number | string,
-    // });
-    // // state.name = 999; // This will throw an error because name is expected to be a string
-    // state.age = 30;
-    // return { ...toRefs(state) };
-
-    // const name = ref("Link");
-    // // const age = ref(25 as number | string);
-    // const age = ref<number | string>(25);
-    // return {
-    //   name,
-    //   age,
-    // };
-
     const jobs = ref<Job[]>([
       {
         id: "1",
@@ -62,14 +37,6 @@ export default defineComponent({
     return {
       jobs,
     };
-  },
-  methods: {
-    // changeName(name: string) {
-    //   this.name = name;
-    // },
-    // changeAge(age: number) {
-    //   this.age = age;
-    // },
   },
 });
 </script>
---
+++ src/components/JobList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+            voluptatibus, cumque, quibusdam, voluptates quisquam voluptatibus
+            cumque quibusdam voluptates quisquam voluptatibus cumque quibusdam
+            voluptates.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "@/types/Job";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      type: Array as PropType<Job[]>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    console.log("props", props.jobs);
+  },
+});
+</script>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
